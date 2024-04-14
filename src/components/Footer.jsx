b613156df8a064d5d3df3b4766702d7eb5de806c
Javier Veron <javier.veron@gmail.com>
2024-04-14 01:23:07 -0300

    Fix Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <div className="container-fluid">
             <div className="row bg-black mt-5 p-3">
                 <div className="col textoFooter text-end">
-                    <p>Copyrigth NEGRO TM - 2024. Todos los derechos reservados. Defensa de las y los consumidores. Para reclamos <b>ingresá acá</b>. / <b>Botón de arrepentimiento</b></p>
+                    <p>Copyright NEGRO TM - 2024. Todos los derechos reservados. Defensa de las y los consumidores. Para reclamos <b>ingresá acá</b>. / <b>Botón de arrepentimiento</b></p>
                 </div>
             </div>
         </div>
