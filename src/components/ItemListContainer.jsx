694a746fd7d656eb6eb0f23b1849099e9155a1e5
Javier Veron <javier.veron@gmail.com>
2024-05-13 22:47:43 -0300

    Firebase 1

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,14 +1,16 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import arrayProductos from "./json/productos.json";
+//import arrayProductos from "./json/productos.json";
 import ItemList from "./ItemList";
 import Carousel from "./Carousel";
+import { collection, doc, getDoc, getDocs, getFirestore, query, where } from "firebase/firestore";
 
 const ItemListContainer = () => {
     const [items, setItems] = useState([]);
     const {id} = useParams();
 
-    useEffect(() => {
+    // Acceso a Productos vía JSON
+    /* useEffect(() => {
         const promesa = new Promise(resolve => {
             setTimeout(() => {
                 resolve(id ? arrayProductos.filter(item => item.categoria == id) : arrayProductos);
@@ -18,7 +20,49 @@ const ItemListContainer = () => {
         promesa.then(respuesta => {
             setItems(respuesta);
         })
-    }, [id])
+    }, [id]) */
+
+    // Acceso a un Producto por ID en Firestore
+    // .id => Accedo al ID del Documento
+    // .data() => Accedo a todos los campos del Documento
+    /* useEffect(() => {
+        const db = getFirestore();
+        const docRef = doc(db, "items", "wYi9ZibpHkIWri9S8UdI");
+        getDoc(docRef).then(snapShot => {
+            if (snapShot.exists()) {
+                setItems({id:snapShot.id, ...snapShot.data()});
+            }
+        });
+    }, []); */
+
+    // Acceder a una Collection en Firestore
+    /* useEffect(() => {
+        const db = getFirestore();
+        const itemsCollection = collection(db, "items");
+        getDocs(itemsCollection).then(snapShot => {
+            if (snapShot.size > 0) {
+                setItems(snapShot.docs.map(item => ({id:item.id, ...item.data()})));
+            }
+        });
+    }, []); */
+
+    // Acceder a una Collection en Firestore mediante una Query
+    useEffect(() => {
+        const db = getFirestore();
+        const itemsCollection = collection(db, "items");
+        //const queryCollection = query(itemsCollection, where("precio", ">", 200000));
+        //const queryCollection = query(itemsCollection, where("precio", "<", 200000));
+        //const queryCollection = query(itemsCollection, where("nombre", "==", "Nike Air Force 1 Shadow"));
+        //const queryCollection = query(itemsCollection, where("nombre", "!=", "Nike Air Force 1 Shadow"));
+        const queryCollection = query(itemsCollection, (where("precio", ">", 100000), where("categoria", "==", "zapatillas")));
+        getDocs(queryCollection).then(snapShot => {
+            if (snapShot.size > 0) {
+                setItems(snapShot.docs.map(item => ({id:item.id, ...item.data()})));
+            }
+        });
+    }, []);
+
+
 
     return (
         <>
@@ -26,6 +70,9 @@ const ItemListContainer = () => {
             <div className="container">
                 <div className="row my-5">
                     <ItemList items={items} />
+                    {/* <p>{items.nombre}</p>
+                    <p><img src={items.imagen} alt={items.imagen} /></p>
+                    <p>${items.precio}</p> */}
                 </div>
             </div>
         </>
--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDy-zQV_HbFJbsMduwvihmosIk2223EmLE",
+  authDomain: "coderhouse-54115.firebaseapp.com",
+  projectId: "coderhouse-54115",
+  storageBucket: "coderhouse-54115.appspot.com",
+  messagingSenderId: "1019918519964",
+  appId: "1:1019918519964:web:623e56c912e425d3052759"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
