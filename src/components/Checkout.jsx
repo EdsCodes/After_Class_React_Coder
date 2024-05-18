6a3610e97470cd927fc7fc6428433965614c9be7
Javier Veron <javier.veron@gmail.com>
2024-05-17 22:17:48 -0300

    After 3

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,83 +1,41 @@
-import { useEffect, useState } from "react";
-//import arrayProductos from "./json/productos.json";
-import { addDoc, collection, doc, getDocs, getFirestore, updateDoc, writeBatch } from "firebase/firestore";
+import { useContext, useEffect, useState } from "react";
+import { CartContext } from "./context/CartContext";
+import { addDoc, collection, doc, getDocs, getFirestore } from "firebase/firestore";
+import { Link } from "react-router-dom";
 
 const Checkout = () => {
-    const [cart, setCart] = useState([]);
+    const {cart, getCountProducts, getSumProducts} = useContext(CartContext);
     const [nombre, setNombre] = useState("");
     const [email, setEmail] = useState("");
     const [telephone, setTelephone] = useState("");
     const [orderId, setOrderId] = useState("");
 
-    // Cargo los productos vía JSON
-    /* useEffect(() => {
-        const promesa = new Promise(resolve => {
-            setTimeout(() => {
-                resolve(arrayProductos.filter(item => item.precio < 40000));
-            }, 2000)
-        });
-        
-        promesa.then(respuesta => {
-            setCart(respuesta);
-        })
-    }, []); */
-
-    // Cargo los productos vía FireStore
-    useEffect(() => {
-        const db = getFirestore();
-        const itemsCollection = collection(db, "items");
-        getDocs(itemsCollection).then(snapShot => {
-            if (snapShot.size > 0) {
-                setCart(snapShot.docs.map(item => ({id:item.id, ...item.data()})));
-            }
-        });
-    }, []);
-
-
-    const calcularTotal = () => {
-        return cart.reduce((acumulador, item) => acumulador += item.precio, 0);
-    }
-
     const generarOrden = () => {
         const buyer = {nombre:nombre, email:email, telephone:telephone};
         const items = cart.map(item => ({id:item.id, title:item.nombre, price:item.precio}));
-        const order = {buyer:buyer, items:items, total:calcularTotal()};
-        //console.log(order);
+        const date = new Date();
+        const currentDate = `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+        const order = {buyer:buyer, items:items, date:currentDate, total:getSumProducts()};
         
-        // Agrego un nuevo Documento al Firestore
-        /* const db = getFirestore();
+        // Agrego un nuevo Documento a la Colección Orders
+        const db = getFirestore();
         const ordersCollection = collection(db, "orders");
         addDoc(ordersCollection, order).then(data => {
             setOrderId(data.id);
-        }); */
+        });
+    }
 
-        // Para agregar todos los productos mi array JSON a mi BD Firestore
-        /* const db = getFirestore();
-        const itemsCollection = collection(db, "items");
-        arrayProductos.forEach(item => {
-            addDoc(itemsCollection, item);
-        })
-        console.log("Proceso de carga productos completo!"); */
-
-        // Modificar un Documento por ID en Firestore
-        /* const db = getFirestore();
-        const orderDoc = doc(db, "items", "8zsqSha4k6kT8MFPv6XM");
-        updateDoc(orderDoc, {precio:55000, nombre:"Taza Cafetera Porteña", precioPromocional:45000}).then(data => {
-            console.log("El Producto se actualizó correctamente!");
-        }) */
-
-
-        // Modificar más de un Document con Batch en Firestore
-        /* const db = getFirestore();
-        const doc1 = doc(db, "items", "8zsqSha4k6kT8MFPv6XM");
-        const doc2 = doc(db, "items", "AmZ5MMrY503MKofHkODR");
-        const doc3 = doc(db, "items", "OFRHGr6uqon87Qzvznwz");
-        const batch = writeBatch(db);
-        batch.update(doc1, {stock:2});
-        batch.update(doc2, {stock:20});
-        batch.set(doc3, {stock:2});
-        batch.commit();
-        console.log("Productos actualizados!"); */
+    if (getCountProducts() == 0) {
+        return (
+            <div className="container my-5">
+                <div className="row">
+                    <div className="col text-center">
+                        <h3>No se encontraron Productos en el Carrito!</h3>
+                        <Link to={"/"} className="btn text-white bg-dark rounded-0 my-5">Volver a la Página Principal</Link>
+                    </div>
+                </div>
+            </div>
+        )
     }
 
     return (
@@ -108,12 +66,13 @@ const Checkout = () => {
                                 <tr key={item.id}>
                                     <td><img src={item.imagen} alt={item.nombre} width={80} /></td>
                                     <td>{item.nombre}</td>
+                                    <td>x{item.quantity}</td>
                                     <td className="text-end">${item.precio}</td>
                                 </tr>
                             ))}
                             <tr>
-                                <td colSpan={2}><b>Total</b></td>
-                                <td className="text-end"><b>${calcularTotal()}</b></td>
+                                <td colSpan={3}><b>Total</b></td>
+                                <td className="text-end"><b>${getSumProducts()}</b></td>
                             </tr>
                         </tbody>
                     </table>
--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,78 +1,34 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-//import arrayProductos from "./json/productos.json";
 import ItemList from "./ItemList";
 import Carousel from "./Carousel";
-import { collection, doc, getDoc, getDocs, getFirestore, query, where } from "firebase/firestore";
+import { collection, getDocs, getFirestore, query, where } from "firebase/firestore";
+import Loading from "./Loading";
 
 const ItemListContainer = () => {
     const [items, setItems] = useState([]);
+    const [visible, setVisible] = useState(true);
     const {id} = useParams();
-
-    // Acceso a Productos vía JSON
-    /* useEffect(() => {
-        const promesa = new Promise(resolve => {
-            setTimeout(() => {
-                resolve(id ? arrayProductos.filter(item => item.categoria == id) : arrayProductos);
-            }, 2000)
-        });
-        
-        promesa.then(respuesta => {
-            setItems(respuesta);
-        })
-    }, [id]) */
-
-    // Acceso a un Producto por ID en Firestore
-    // .id => Accedo al ID del Documento
-    // .data() => Accedo a todos los campos del Documento
-    /* useEffect(() => {
-        const db = getFirestore();
-        const docRef = doc(db, "items", "wYi9ZibpHkIWri9S8UdI");
-        getDoc(docRef).then(snapShot => {
-            if (snapShot.exists()) {
-                setItems({id:snapShot.id, ...snapShot.data()});
-            }
-        });
-    }, []); */
-
-    // Acceder a una Collection en Firestore
-    /* useEffect(() => {
-        const db = getFirestore();
-        const itemsCollection = collection(db, "items");
-        getDocs(itemsCollection).then(snapShot => {
-            if (snapShot.size > 0) {
-                setItems(snapShot.docs.map(item => ({id:item.id, ...item.data()})));
-            }
-        });
-    }, []); */
 
     // Acceder a una Collection en Firestore mediante una Query
     useEffect(() => {
         const db = getFirestore();
         const itemsCollection = collection(db, "items");
-        //const queryCollection = query(itemsCollection, where("precio", ">", 200000));
-        //const queryCollection = query(itemsCollection, where("precio", "<", 200000));
-        //const queryCollection = query(itemsCollection, where("nombre", "==", "Nike Air Force 1 Shadow"));
-        //const queryCollection = query(itemsCollection, where("nombre", "!=", "Nike Air Force 1 Shadow"));
-        const queryCollection = query(itemsCollection, (where("precio", ">", 100000), where("categoria", "==", "zapatillas")));
+        const queryCollection = id ? query(itemsCollection, where("categoria", "==", id)) : itemsCollection;
         getDocs(queryCollection).then(snapShot => {
             if (snapShot.size > 0) {
                 setItems(snapShot.docs.map(item => ({id:item.id, ...item.data()})));
+                setVisible(false);
             }
         });
-    }, []);
-
-
+    }, [id]);
 
     return (
         <>
             {id ? "" : <Carousel />}
             <div className="container">
                 <div className="row my-5">
-                    <ItemList items={items} />
-                    {/* <p>{items.nombre}</p>
-                    <p><img src={items.imagen} alt={items.imagen} /></p>
-                    <p>${items.precio}</p> */}
+                    {visible ? <Loading /> : <ItemList items={items} />}
                 </div>
             </div>
         </>
